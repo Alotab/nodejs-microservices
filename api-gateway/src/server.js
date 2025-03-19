e1a1064ce7c3e28ffc77133f95bab976962b6505
Albert Osei Frimpong <alotab@yahoo.com>
2025-03-19 04:09:39 -0700

    fix redis error

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -7,13 +7,13 @@ const { rateLimit } = require("express-rate-limit");
 const { RedisStore } = require("rate-limit-redis");
 const logger = require("./utils/logger");
 const proxy = require("express-http-proxy");
-const errorHandler = require("./middleware/errorhandler");
-const { validateToken } = require("./middleware/authMiddleware");
+const errorHandler = require("./middleware/errorHandler");
+const { validateToken } = require("./middleware/authMiddleware")
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const redisClient = new Redis(process.env.REDIS_URL);
+// const redisClient = new Redis(process.env.REDIS_URL);
 
 app.use(helmet());
 app.use(cors());
@@ -29,12 +29,12 @@ const ratelimitOptions = rateLimit({
     logger.warn(`Sensitive endpoint rate limit exceeded for IP: ${req.ip}`);
     res.status(429).json({ success: false, message: "Too many requests" });
   },
-  store: new RedisStore({
-    sendCommand: (...args) => redisClient.call(...args),
-  }),
+  // store: new RedisStore({
+  //   // sendCommand: (...args) => redisClient.call(...args),
+  // }),
 });
 
-app.use(ratelimitOptions);
+// app.use(ratelimitOptions);
 
 app.use((req, res, next) => {
   logger.info(`Received ${req.method} request to ${req.url}`);
@@ -75,73 +75,73 @@ app.use(
 );
 
 // setting up proxy for our post service
-app.use(
-  "/v1/posts",
-  validateToken,
-  proxy(process.env.POST_SERVICE_URL, {
-    ...proxyOptions,
-    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
-      proxyReqOpts.headers["Content-Type"] = "application/json";
-      proxyReqOpts.headers["x-user-id"] = srcReq.user.userId;
+// app.use(
+//   "/v1/posts",
+//   validateToken,
+//   proxy(process.env.POST_SERVICE_URL, {
+//     ...proxyOptions,
+//     proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+//       proxyReqOpts.headers["Content-Type"] = "application/json";
+//       proxyReqOpts.headers["x-user-id"] = srcReq.user.userId;
 
-      return proxyReqOpts;
-    },
-    userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
-      logger.info(
-        `Response received from Post service: ${proxyRes.statusCode}`
-      );
+//       return proxyReqOpts;
+//     },
+//     userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+//       logger.info(
+//         `Response received from Post service: ${proxyRes.statusCode}`
+//       );
 
-      return proxyResData;
-    },
-  })
-);
+//       return proxyResData;
+//     },
+//   })
+// );
 
 // setting up proxy for our media service
-app.use(
-  "/v1/media",
-  validateToken,
-  proxy(process.env.MEDIA_SERVICE_URL, {
-    ...proxyOptions,
-    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
-      proxyReqOpts.headers["x-user-id"] = srcReq.user.userId;
-      if (!srcReq.headers["content-type"].startsWith("multipart/form-data")) {
-        proxyReqOpts.headers["Content-Type"] = "application/json";
-      }
+// app.use(
+//   "/v1/media",
+//   validateToken,
+//   proxy(process.env.MEDIA_SERVICE_URL, {
+//     ...proxyOptions,
+//     proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+//       proxyReqOpts.headers["x-user-id"] = srcReq.user.userId;
+//       if (!srcReq.headers["content-type"].startsWith("multipart/form-data")) {
+//         proxyReqOpts.headers["Content-Type"] = "application/json";
+//       }
 
-      return proxyReqOpts;
-    },
-    userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
-      logger.info(
-        `Response received from media service: ${proxyRes.statusCode}`
-      );
+//       return proxyReqOpts;
+//     },
+//     userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+//       logger.info(
+//         `Response received from media service: ${proxyRes.statusCode}`
+//       );
 
-      return proxyResData;
-    },
-    parseReqBody: false,
-  })
-);
+//       return proxyResData;
+//     },
+//     parseReqBody: false,
+//   })
+// );
 
 // setting up proxy for our search service
-app.use(
-  "/v1/search",
-  validateToken,
-  proxy(process.env.SEARCH_SERVICE_URL, {
-    ...proxyOptions,
-    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
-      proxyReqOpts.headers["Content-Type"] = "application/json";
-      proxyReqOpts.headers["x-user-id"] = srcReq.user.userId;
+// app.use(
+//   "/v1/search",
+//   validateToken,
+//   proxy(process.env.SEARCH_SERVICE_URL, {
+//     ...proxyOptions,
+//     proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+//       proxyReqOpts.headers["Content-Type"] = "application/json";
+//       proxyReqOpts.headers["x-user-id"] = srcReq.user.userId;
 
-      return proxyReqOpts;
-    },
-    userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
-      logger.info(
-        `Response received from Search service: ${proxyRes.statusCode}`
-      );
+//       return proxyReqOpts;
+//     },
+//     userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+//       logger.info(
+//         `Response received from Search service: ${proxyRes.statusCode}`
+//       );
 
-      return proxyResData;
-    },
-  })
-);
+//       return proxyResData;
+//     },
+//   })
+// );
 
 app.use(errorHandler);
 
@@ -150,14 +150,14 @@ app.listen(PORT, () => {
   logger.info(
     `Identity service is running on port ${process.env.IDENTITY_SERVICE_URL}`
   );
-  logger.info(
-    `Post service is running on port ${process.env.POST_SERVICE_URL}`
-  );
-  logger.info(
-    `Media service is running on port ${process.env.MEDIA_SERVICE_URL}`
-  );
-  logger.info(
-    `Search service is running on port ${process.env.SEARCH_SERVICE_URL}`
-  );
+  // logger.info(
+  //   `Post service is running on port ${process.env.POST_SERVICE_URL}`
+  // );
+  // logger.info(
+  //   `Media service is running on port ${process.env.MEDIA_SERVICE_URL}`
+  // );
+  // logger.info(
+  //   `Search service is running on port ${process.env.SEARCH_SERVICE_URL}`
+  // );
   logger.info(`Redis Url ${process.env.REDIS_URL}`);
 });
