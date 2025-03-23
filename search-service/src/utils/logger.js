1b09a42f1b2cfe27d7eb3856211f5830c94b673b
Albert Osei Frimpong <alotab@yahoo.com>
2025-03-23 03:40:29 -0700

    Search Service

--- search-service/src/utils/logger.js
+++ search-service/src/utils/logger.js
@@ -1,40 +1,24 @@
-const amqp = require("amqplib");
-const logger = require("./logger");
+const winston = require("winston");
 
-let connection = null;
-let channel = null;
+const logger = winston.createLogger({
+  level: process.env.NODE_ENV === "production" ? "info" : "debug",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.errors({ stack: true }),
+    winston.format.splat(),
+    winston.format.json()
+  ),
+  defaultMeta: { service: "post-service" },
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
+});
 
-const EXCHANGE_NAME = "facebook_events";
-
-async function connectToRabbitMQ() {
-  try {
-    connection = await amqp.connect(process.env.RABBITMQ_URL);
-    channel = await connection.createChannel();
-
-    await channel.assertExchange(EXCHANGE_NAME, "topic", { durable: false });
-    logger.info("Connected to rabbit mq");
-    return channel;
-  } catch (e) {
-    logger.error("Error connecting to rabbit mq", e);
-  }
-}
-
-async function consumeEvent(routingKey, callback) {
-  if (!channel) {
-    await connectToRabbitMQ();
-  }
-
-  const q = await channel.assertQueue("", { exclusive: true });
-  await channel.bindQueue(q.queue, EXCHANGE_NAME, routingKey);
-  channel.consume(q.queue, (msg) => {
-    if (msg !== null) {
-      const content = JSON.parse(msg.content.toString());
-      callback(content);
-      channel.ack(msg);
-    }
-  });
-
-  logger.info(`Subscribed to event: ${routingKey}`);
-}
-
-module.exports = { connectToRabbitMQ, consumeEvent };
+module.exports = logger;
